df240a759877eebce51ca633698fbfda30e5d0f7
Arnold Daniels <arnold@jasny.net>
2016-11-24 13:54:02 +0100

    In Dutch only add acute to 'een' if it's a word on it's own

--- test/nl.spec.js
+++ test/nl.spec.js
@@ -13,6 +13,11 @@ describe('spell-it in Dutch', function(){
 		expect(output).to.equal('nul');
 	});
 
+	it('should return "één" when input is 1', function(){
+		var output = spell(1);
+		expect(output).to.equal('één');
+	});
+
 	it('should return "vijf" if input is 5', function(){
 		var output = spell(5);
 		expect(output).to.equal('vijf');
@@ -25,7 +30,7 @@ describe('spell-it in Dutch', function(){
 
 	it('should combine compound numbers from twenty-one through ninety-nine', function(){
 		var output = spell(21);
-		expect(output).to.equal('éénentwintig');
+		expect(output).to.equal('eenentwintig');
 	});
 
 	it('should add a dieresis for compound numbers', function(){
@@ -53,18 +58,18 @@ describe('spell-it in Dutch', function(){
 		expect(output).to.equal('driehonderdtwaalfduizend vijfhonderdzevenenzeventig');
 	});
 
-	it('should return "één miljoen tweehonderdduizend één" if input is 1200001', function(){
-		var output = spell(1200001);
-		expect(output).to.equal('één miljoen tweehonderdduizend één');
+	it('should return "één miljoen tweehonderdduizend één" if input is 1200002', function(){
+		var output = spell(1200002);
+		expect(output).to.equal('één miljoen tweehonderdduizend twee');
 	});
 
-	it('should return "één miljard vijf miljoen tweehonderdduizend één" if input is 1005200001', function(){
-		var output = spell(1005200001);
-		expect(output).to.equal('één miljard vijf miljoen tweehonderdduizend één');
+	it('should return "één miljard vijf miljoen tweehonderdduizend één" if input is 1005200002', function(){
+		var output = spell(1005200002);
+		expect(output).to.equal('één miljard vijf miljoen tweehonderdduizend twee');
 	});
 
-	it('should return "vijf miljard vijf miljoen tweehonderdduizend één" if input is 5005200001', function(){
-		var output = spell(5005200001);
-		expect(output).to.equal('vijf miljard vijf miljoen tweehonderdduizend één');
+	it('should return "vijf miljard vijf miljoen tweehonderdduizend één" if input is 5005200002', function(){
+		var output = spell(5005200002);
+		expect(output).to.equal('vijf miljard vijf miljoen tweehonderdduizend twee');
 	});
 });
