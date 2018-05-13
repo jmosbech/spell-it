03296e81509b9a23a2e438b29f0a25c70cc2cd48
Jonas Mosbech <jonas@mosbech.net>
2018-05-13 21:28:17 +0200

    Fix cli

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 
-var spell = require('spell-it')(process.argv[3]);
+var spell = require('./lib/index.js')(process.argv[3]);
 
 var input = parseInt(process.argv[2], 10);
 console.log(spell(input));
